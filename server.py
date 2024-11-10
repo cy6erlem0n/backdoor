619fabe5f1b944f2336eebde600b73f32219c139
cy6erlem0n <dmytriy.drozd.2014@gmail.com>
2024-11-10 21:14:04 +0100

    version 1.1

--- server.py
+++ server.py
@@ -32,6 +32,8 @@ def server():
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind(("127.0.0.1",54321))
+        s.listen(1)
+        target, ip = s.accept()
 
 server()
 shell()
